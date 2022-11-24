666b0c5ed86a0b4cfedb042c8c94703e135b233b
MercyNaima <58659452+MercyNaima@users.noreply.github.com>
2022-11-24 18:46:33 +0800

    add mercy_math.py and update __init__.py
    1. add mercy_math.py
    2. update __init__.py

---
+++ MercyStudy/__init__.py
@@ -0,0 +1,10 @@
+# -- coding: utf-8 --
+from . import dec_convert
+from . import get_num
+from . import code_convert
+from . import mercy_decorators
+from . import mercy_math
+
+
+__all__ = ['dec_convert', 'get_num', 'code_convert', 'mercy_decorators', 'mercy_math']
+
